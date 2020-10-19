57d7c0c32f1991b9af3705d6760bcd5e358fcd72
Edyta Bednarz <edy.b.bednarz@gmail.com>
2020-10-19 17:55:55 +0100

    Displaying list in index.html

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
-
 from todo_app.flask_config import Config
+from flask import render_template
+from todo_app.data import session_items
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -8,7 +9,9 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    items = session_items.get_items()
+    print(items)
+    return render_template('index.html', items = items)
 
 
 if __name__ == '__main__':
