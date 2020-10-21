06b2ef4aad73bfe860349e6362bbe5c4623eec27
Edyta Bednarz <edy.b.bednarz@gmail.com>
2020-10-21 17:31:04 +0100

    Added add_item functionality

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,5 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, request
 from todo_app.flask_config import Config
-from flask import render_template
 from todo_app.data import session_items
 
 app = Flask(__name__)
@@ -10,9 +9,21 @@ app.config.from_object(Config)
 @app.route('/')
 def index():
     items = session_items.get_items()
-    
+
     print(items)
     return render_template('index.html', items=items)
+
+@app.route('/add', methods=['GET'])
+def add():
+    return render_template('form.html')
+
+@app.route('/add', methods=['POST'])
+def post_item():
+    title = request.form.get('add')
+    new_item = session_items.add_item(title=title)
+    return redirect('/')
+
+
 
 if __name__ == '__main__':
     app.run()
